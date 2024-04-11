081fb4abaaa438db69a1c8bcdb871f0cb78f0a4e
jihunp <edwardpark.park@gmail.com>
2024-04-10 18:04:19 -0700

    moveable element on screen

---
+++ app/javascript/controllers/drag_and_drop_controller.js
@@ -0,0 +1,31 @@
+import {Controller} from "@hotwired/stimulus";
+//make a trash can if items are hovered above the trashcan then allow the item delete to be deleted.
+export default class extends Controller {
+  connect() {
+  }
+  initialize() {
+    this.isDragging = false;
+    this.xOffset = 0;
+    this.yOffset = 0;
+  }
+  mousedown(event) {
+    this.isDragging = true;
+    this.initialX = event.clientX - this.xOffset;
+    this.initialY = event.clientY - this.yOffset;
+    console.log("mouse down has been activated");
+  }
+  mouseup() {
+    this.isDragging = false;
+  }
+  mousemove(event) {
+    if (!this.isDragging) {
+      return;
+    }
+
+    this.xOffset = event.clientX - this.initialX;
+    this.yOffset = event.clientY - this.initialY;
+
+    this.element.style.transform = `translate(${this.xOffset}px ${this.yOffset}px)`;
+    //transforms the style of the current box
+  }
+}
